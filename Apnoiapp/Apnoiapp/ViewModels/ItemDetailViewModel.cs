cea0f81c4d98a10c0114192d45ba6e31a905e24d
AndreasValeroG <abenjamin.v@gmail.com>
2019-09-10 12:45:18 -0300

    Agregar archivos de proyecto.

---
+++ Apnoiapp/Apnoiapp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Apnoiapp.Models;
+
+namespace Apnoiapp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Apnoiapp/Apnoiapp/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Apnoiapp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
