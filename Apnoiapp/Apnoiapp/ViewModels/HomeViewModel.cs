373953f7c7f0c0aebddcd92726df94be412ba7c7
AndreasValeroG <abenjamin.v@gmail.com>
2019-10-28 16:44:53 -0300

    [ADD] Bottom frame for inscriptions on HomePage

--- Apnoiapp/Apnoiapp/ViewModels/HomeViewModel.cs
+++ Apnoiapp/Apnoiapp/ViewModels/HomeViewModel.cs
@@ -9,6 +9,8 @@ namespace Apnoiapp.ViewModels
     public class HomeViewModel
     {
         public ICommand ScheduleCommand { get; set; }
+        public ICommand FinswimmingRegistrationFormCommand { get; set; }
+        public ICommand GoToEventsPageCommand { get; set; }
 
         public HomeViewModel()
         {
@@ -21,6 +23,24 @@ namespace Apnoiapp.ViewModels
 
                 if (Device.RuntimePlatform == Device.Android)
                     await Task.Delay(100);
+            });
+
+            GoToEventsPageCommand = new Command(async () =>
+            {
+                var id = (int)MenuItemType.Events;
+
+                var mainPage = (App.Current.MainPage as MainPage);
+                await mainPage.NavigateFromMenu(id);
+
+                if (Device.RuntimePlatform == Device.Android)
+                    await Task.Delay(100);
+            });
+
+            FinswimmingRegistrationFormCommand = new Command(async () =>
+            {
+                Page fsform = new Views.RegistrationForms.FinswimmingRegistrationPage();
+
+                await Application.Current.MainPage.Navigation.PushModalAsync(fsform);
             });
         }
     }
